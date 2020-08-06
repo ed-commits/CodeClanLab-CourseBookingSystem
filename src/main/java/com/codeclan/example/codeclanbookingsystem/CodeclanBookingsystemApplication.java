21aeabba840eb3faac31ead256c0bc1baa5e7929
ed-commits <edward.alexander.cant@gmail.com>
2020-08-06 09:31:58 +0100

    import

---
+++ src/main/java/com/codeclan/example/codeclanbookingsystem/CodeclanBookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.codeclanbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeclanBookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeclanBookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/codeclanbookingsystem/CodeclanBookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.codeclanbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeclanBookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
