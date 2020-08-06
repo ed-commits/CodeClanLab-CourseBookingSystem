95532d8007ed3b00017ba3f3d62862ec4587f70e
ed-commits <edward.alexander.cant@gmail.com>
2020-08-06 11:29:35 +0100

    bookings controller

--- src/main/java/com/codeclan/example/codeclanbookingsystem/Controllers/BookingController.java
+++ src/main/java/com/codeclan/example/codeclanbookingsystem/Controllers/BookingController.java
@@ -1,4 +1,26 @@
 package com.codeclan.example.codeclanbookingsystem.Controllers;
 
+import com.codeclan.example.codeclanbookingsystem.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/bookings")
+    public ResponseEntity getBookingsForADate(
+            @RequestParam(required = false, name = "date") String date
+    ) {
+        if(date != null) {
+            return new ResponseEntity<>(bookingRepository.findBookingsByDate(date), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
 }
